f4b9c3f7a1d869b739723de5d4c32756e0b91be0
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2020-11-18 17:23:04 +0200

    webpack course

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,59 +1,8 @@
-const fs = require("fs");
 const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-
-let plugins = [];
-const copyPluginFrom = "mocks";
-const copyPluginTo = "configurations";
-
-function copyPluginFilter(resourcePath) {
-    let pathAsArray = resourcePath.split(path.sep);
-    let fileName = pathAsArray[pathAsArray.length - 1];
-    // filter out data
-    return fileName.match(/.json$/gm) && !fileName.match(/data/gm);
-}
-
-async function copyPluginTransformPath(targetPath, resourcePath) {
-    let pathAsArray = resourcePath.split(path.sep);
-    let fileName = pathAsArray[pathAsArray.length - 1];
-    // mixin without id in json workaround (use name)
-    let tempId = fileName.split(".");
-    let data = await fs.promises.readFile(resourcePath);
-    let stringData = data.toString();
-    let json = JSON.parse(stringData);
-    let id = json.id || tempId[0];
-    let targetPathArray = targetPath.split(path.sep);
-    return targetPathArray[0] + path.sep + targetPathArray[1] + path.sep + id;
-}
-
-plugins.push(
-    new CopyPlugin({
-        patterns: [
-            {
-                from: copyPluginFrom + path.sep + "dashboards",
-                to: copyPluginTo + path.sep + "dashboards",
-                filter: copyPluginFilter,
-                transformPath: copyPluginTransformPath
-            },
-            {
-                from: copyPluginFrom + path.sep + "widgets",
-                to: copyPluginTo + path.sep + "widgets",
-                filter: copyPluginFilter,
-                transformPath: copyPluginTransformPath
-            },
-            {
-                from: copyPluginFrom + path.sep + "mixins",
-                to: copyPluginTo + path.sep + "mixins",
-                filter: copyPluginFilter,
-                transformPath: copyPluginTransformPath
-            }
-        ]
-    })
-);
 
 module.exports = {
     entry: {
-        main: "./src/main.js"
+        main: ["./src/main.js"]
     },
     mode: "development",
     output: {
@@ -61,7 +10,6 @@ module.exports = {
         path: path.resolve(__dirname, "../dist"),
         publicPath: "/"
     },
-    plugins,
     devServer: {
         contentBase: "dist",
         overlay: true
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+require("core-js/es/promise");
+require("@babel/runtime/regenerator");
 require("./main.css");
 require("./index.html");
 
-let a = () => {
-    console.log("Hello from the future!");
+let a = async (args) => {
+    const { a, b } = args;
+    await console.log("Hello from the future!", a, b);
+    console.log("Done");
 };
+
+a({ a: 1, b: 2 });
